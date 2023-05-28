941b436c0364e0a0eaf74cf28d7e4e3e36e6a860
Joe Parrinello <joe@parrinello.me>
2023-05-28 19:53:00 +0000

    Init Commit: Add types and Setup Webhooks

---
+++ main.py
@@ -0,0 +1,69 @@
+"""StonksBot CloudFunction."""
+import os
+from flask import jsonify
+import functions_framework
+import requests
+from google.cloud import secretmanager
+from discord_types import ApplicationCommandType, ApplicationCommandOptionType, InteractionType, InteractionCallbackType
+
+
+project_id = os.environ["PROJECT_ID"]
+client = secretmanager.SecretManagerServiceClient()
+name = f"projects/{project_id}/secrets/stonksbot_app_id/versions/latest"
+response = client.access_secret_version(name=name)
+stonksbot_app_id = response.payload.data.decode("UTF-8")
+
+@functions_framework.http
+def discord_webhook(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    """
+    request_json = request.get_json(silent=True)
+
+    if request_json and 'type' in request_json and request_json["type"] == InteractionType.PING:
+        return jsonify({
+            "type": InteractionCallbackType.PONG
+        })
+    return jsonify({
+        "type": InteractionCallbackType.PONG
+    })
+
+
+@functions_framework.http
+def register_bot(request):
+    """HTTP Cloud Function
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The response text, or any set of values that can be turned into a
+        Response object using `make_response`
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    """
+
+    url = "https://discord.com/api/v10/applications/%s/commands" % stonksbot_app_id
+
+    json = {
+        "name": "stonks",
+        "type": ApplicationCommandType.CHAT_INPUT,
+        "description": "Request the price info on a stock",
+        "options": [
+            {
+                "name": "ticker",
+                "description": "The stock ticker as listed on the market.",
+                "type": ApplicationCommandOptionType.STRING,
+                "required": True,
+            }
+        ]
+    }
+    headers = {
+        "Authorization": "Bot <my_bot_token>"
+    }
+    requests.post(url, headers=headers, json=json, timeout=30)
+    return "Success", 200
